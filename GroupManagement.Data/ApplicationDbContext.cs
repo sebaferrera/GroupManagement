d6ca8c28629290e201195646c0d58b5e0d186afc
sferrera <sebastian.ferrera@rindus.de>
2020-11-12 16:47:30 +0100

    DbSets

--- GroupManagement.Data/ApplicationDbContext.cs
+++ GroupManagement.Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GroupManagement.Models.Masters;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ namespace GroupManagement.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
